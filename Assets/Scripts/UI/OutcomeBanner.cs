5fc3027a5bf4c6965828fc2804efe97b59e9f0b6
Denzel Joaquin Tano <denzel_tano@yahoo.com>
2021-12-01 07:55:17 +0700

    Added several sprites
    - Tick
    - X
    - A box

--- Assets/Scripts/UI/OutcomeBanner.cs
+++ Assets/Scripts/UI/OutcomeBanner.cs
@@ -10,7 +10,17 @@ public class OutcomeBanner : MonoBehaviour
     [SerializeField] Image customerSprite;
     [SerializeField] TextMeshProUGUI customerName;
     [SerializeField] TextMeshProUGUI survivalProbability;
-    [SerializeField] Image hasSurvivedSprite; 
+    [SerializeField] Image hasSurvivedSprite;
+
+    [SerializeField] private Sprite successSprite;
+    [SerializeField] private Sprite failureSprite;
+
+    void Awake()
+    {
+        //successSprite = Resources.Load("Sprites/Tick") as Sprite;
+    }
+
+
     
     public void SetBanner(CustomerData custData, bool hasSurvived)
     {
@@ -19,7 +29,12 @@ public class OutcomeBanner : MonoBehaviour
         survivalProbability.text = $"{custData.SurvivalProbability.ToString()}%";
 
         if(!hasSurvived){
-            hasSurvivedSprite.enabled = false;
+            //hasSurvivedSprite.enabled = false;
+            hasSurvivedSprite.sprite = failureSprite;
+            hasSurvivedSprite.color = Color.red;
+        }else{
+            hasSurvivedSprite.sprite = successSprite;
+            hasSurvivedSprite.color = Color.white;
         }
 
     }
